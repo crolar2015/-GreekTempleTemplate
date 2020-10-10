23dbf992370e82e0e99018446835a694af3119a4
Larry Crochet <crolar2015@gmail.com>
2020-10-09 21:55:23 -0400

    add lions
    Former-commit-id: 5781bea4d4a35ff8ea009827cd9f854e963e62ce

--- Assets/Plugins/upload.bat
+++ Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\crola\Documents\unity3d\unityprojects\crolar template 2\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1579447268644225659.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\crola\Documents\unity3d\unityprojects\crolar template 2\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1580129214047191628.json
